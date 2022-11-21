276a6781ab0eb490609033fffeb8d56c7b6b6379
Mauricio <mauricioaguilera5555@gmail.com>
2022-11-21 16:37:35 -0300

    Se agregaron class base views de blog

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,72 @@
 from django.shortcuts import render
+from blog.models import Configuracion
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
+from blog.models import Post
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.admin import User
+
 
 def index(request):
     return render(request, 'blog/index.html')
 
-def hola():
-    print ("hola")
+@login_required
+def index(request):
+    configuracion=Configuracion.objects.first()
+    return render(request, 'blog/index.html', {'configuracion':configuracion})
+
+class ListPost(LoginRequiredMixin, ListView):
+    model=Post
+
+
+class CreatePost(CreateView):
+    model=Post
+    fields = ['title', 'short_content', 'content', 'image']
+    success_url = reverse_lazy("list-post")
+    
+class DetailPost(DetailView):
+    model=Post
+
+class UpdatePost(UpdateView):
+    model=Post
+    fields=['title', 'short_content', 'content', 'image']
+    success_url = reverse_lazy("list-post")
+
+class DeletePost(LoginRequiredMixin ,DeleteView):
+    model=Post
+    success_url = reverse_lazy("list-post")
+
+class SearchPostByName(ListView):
+    def get_queryset(self):
+        blog_title = self.request.GET.get('post-title')
+        return Post.objects.filter(title__icontains=blog_title)
+
+class BlogLogin(LoginView):
+    template_name = 'blog/blog_login.html'
+    next_page = reverse_lazy("list-post")
+
+class BlogLogout(LogoutView):
+    template_name = 'blog/blog_logout.html'
+
+class SearchpostByName(ListView):
+    def get_queryset(self):
+        post_title = self.request.GET.get('post-title')
+        return Post.objects.filter(post__icontains=post_title)
+
+class BlogSignUp(CreateView):
+
+    form_class = UserCreationForm
+
+    success_url = reverse_lazy("blog-login")
+    template_name = "registration/signup.html"
+
+class ProfileUpdate(UpdateView):
+
+    model = User
+
+    fields = ['username', 'first_name', 'last_name', 'email']
+
+    success_url = reverse_lazy("blog-login")
